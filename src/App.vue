e45bc61f2bd4b2f27a6651c267aea7e4b9bba7eb
Neota Moe <neota.moe@gmail.com>
2018-04-08 15:14:00 -0500

    fixed component to be used correctly

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="skills">
-    Skills
+  <div class="app">
+    <Skills />
   </div>
+
 </template>
 
 <script>
@@ -11,7 +12,7 @@ export default {
   name: 'app',
   components: {
     Skills
-  }
+  } 
 }
 </script>
 
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="skills">
-    Skills
+    {{name}}
+   <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-
+  data() {
+    return {
+      name: 'Coursetro tutorial',
+      btnState: true
+    }
   }
 }
 </script>
