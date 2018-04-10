c77e1ed989a305c9e92f59d23b4d093ac081bd0c
Neota Moe <neota.moe@gmail.com>
2018-04-10 07:53:30 -0500

    some css animations

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,7 +4,9 @@
 
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
-        <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        </transition>
       </form>
 
       <ul>
@@ -46,6 +48,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
   .holder {
     background: #fff;
   }
@@ -90,5 +94,25 @@ export default {
     display: inline-block;
     padding: 5px;
     margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
