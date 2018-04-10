4e5d5f8aa52b4f78d5751164f889189a562ea20f
Neota Moe <neota.moe@gmail.com>
2018-04-10 08:14:01 -0500

    routing

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
+  import Skills from './components/Skills.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Skills
-  } 
-}
+  export default {
+    name: 'app',
+    components: {
+      Skills
+    } 
+  }
 </script>
 
 <style>
@@ -25,7 +30,7 @@ export default {
     display: grid;
     grid-template-rows: auto;
     justify-items: center;
-    align-items: center;
+    padding-top: 50px;
   }
   body, html {
     margin: 0;
@@ -33,5 +38,18 @@ export default {
   }
   #app {
       width: 50%;
+  }
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
   }
 </style>
